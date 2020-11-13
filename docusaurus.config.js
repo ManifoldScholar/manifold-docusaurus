5b5f88dc77a54a0524e0aa4c0734448767314211
Zach Davis <zach@castironcoding.com>
2020-11-13 09:43:44 -0800

    Configure github pages deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   title: 'Manifold Docs',
   tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://manifoldscholar.github.io',
+  baseUrl: '/manifold-docusaurus/',
   favicon: 'img/manifold-favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ManifoldScholar', // Usually your GitHub org/user name.
+  projectName: 'manifold-docusaurus', // Usually your repo name.
   themeConfig: {
     footer: {
       logo: {
