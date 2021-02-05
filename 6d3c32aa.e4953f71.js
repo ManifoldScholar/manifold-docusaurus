1fb54673fdaf7b2d94abc0182269fd748505ed3a
jojokarlin <jojo.karlin@gmail.com>
2021-02-05 16:05:50 -0500

    Deploy website - based on d28b3b5bcae0c5a6e282cf60a9a823a480dec892

---
+++ 6d3c32aa.e4953f71.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{86:function(o){o.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/manifold-docusaurus/blog/welcome"},{"title":"Hello","permalink":"/manifold-docusaurus/blog/hello-world"},{"title":"Hola","permalink":"/manifold-docusaurus/blog/hola"}]}')}}]);
