7611c7e2f8998d060f2635d22152d1a3a07e9bd9
Jen Wardwell <57107444+jen-castiron@users.noreply.github.com>
2021-02-17 15:05:42 -0800

    [F] Style docs theme to match new marketing site (#50)
    - Add sass plugin and decorative monospace font
    - Style nav, menu and main content
    - Adjust table of contents padding
    - Adjust link color
    - Add alert styling
    - Adjust table of contents styling
    - Remove logo from footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,68 +1,120 @@
 module.exports = {
-  title: 'Manifold Docs',
-  tagline: 'The tagline of my site',
-  url: 'https://manifoldscholar.github.io',
-  baseUrl: '/manifold-docusaurus/',
-  favicon: 'img/manifold-favicon.ico',
-  organizationName: 'ManifoldScholar', // Usually your GitHub org/user name.
-  projectName: 'manifold-docusaurus', // Usually your repo name.
+  title: "Manifold Docs",
+  tagline: "The tagline of my site",
+  url: "https://manifoldscholar.github.io",
+  baseUrl: "/manifold-docusaurus/",
+  favicon: "img/manifold-favicon.ico",
+  organizationName: "ManifoldScholar", // Usually your GitHub org/user name.
+  projectName: "manifold-docusaurus", // Usually your repo name.
+  plugins: ["docusaurus-plugin-sass"],
   themeConfig: {
     footer: {
-      logo: {
-        alt: 'Manifold Docs',
-        src: '/img/logo.svg',
-        href: '/',
-      },
+      links: [
+        {
+          title: "Platform",
+          items: [
+            {
+              label: "Get Started",
+              href: "https://manifoldapp.org/get-started"
+            },
+            {
+              label: "Features",
+              href: "https://manifoldapp.org/features"
+            },
+            {
+              label: "Services",
+              href: "https://manifoldapp.org/services"
+            }
+          ]
+        },
+        {
+          title: "People",
+          items: [
+            {
+              label: "Community",
+              href: "https://manifoldapp.org/community"
+            },
+            {
+              label: "Blog",
+              to: "blog"
+            },
+            {
+              label: "About",
+              href: "https://manifoldapp.org/history"
+            },
+            {
+              label: "Development",
+              href: "https://manifoldapp.org/development"
+            }
+          ]
+        },
+        {
+          title: "Contact",
+          items: [
+            {
+              label: "Email",
+              href: "mailto:office@castironcoding.com"
+            },
+            {
+              label: "Github",
+              to: "https://github.com/ManifoldScholar"
+            },
+            {
+              label: "Slack",
+              href: "https://slack.com/"
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/manifoldscholar"
+            }
+          ]
+        }
+      ]
     },
     navbar: {
-      logo: {
-        alt: 'Manifold Docs',
-        src: '/img/logo.svg',
-        href: '/',
-      },
       items: [
         {
-          to: 'docs',
-          activeBasePath: 'docs',
-          activeBaseRegex: 'docs/(full)',
-          label: 'Documentation',
-          position: 'left',
+          to: "docs",
+          activeBasePath: "docs",
+          activeBaseRegex: "docs/(full)",
+          label: "Documentation",
+          position: "left"
         },
         {
-          to: 'docs/walkthroughs/landing',
-          activeBasePath: 'docs/walkthroughs',
-          label: 'Walkthroughs',
-          position: 'left',
+          to: "docs/walkthroughs/landing",
+          activeBasePath: "docs/walkthroughs",
+          label: "Walkthroughs",
+          position: "left"
         },
         {
-          to: 'docs/guides/landing',
-          activeBasePath: 'docs/guides',
-          label: 'Guides',
-          position: 'left',
-        },
-      ],
+          to: "docs/guides/landing",
+          activeBasePath: "docs/guides",
+          label: "Guides",
+          position: "left"
+        }
+      ]
     }
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/ManifoldScholar/manifold-docusaurus/edit/master',
+            "https://github.com/ManifoldScholar/manifold-docusaurus/edit/master"
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            "https://github.com/facebook/docusaurus/edit/master/website/blog/"
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
-    ],
-  ],
+          customCss: require.resolve("./src/scss/styles.scss")
+        }
+      }
+    ]
+  ]
 };
