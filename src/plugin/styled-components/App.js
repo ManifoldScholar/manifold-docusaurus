f82480e348442ffc47473b1b6d3856cb4ab55898
jen-castiron <jen@castironcoding.com>
2021-02-19 14:55:15 -0800

    [F] Fix baseURL for builds - Remove theme

--- src/plugin/styled-components/App.js
+++ src/plugin/styled-components/App.js
@@ -1,18 +1,8 @@
-import React from 'react';
+import React from "react";
 import AppOriginal from "AppOriginal";
-import { ThemeProvider } from "styled-components";
 
 function App() {
-
-  const theme = {
-    main: "mediumseagreen"
-  };
-
-  return (
-    <ThemeProvider theme={theme}>
-      <AppOriginal />
-    </ThemeProvider>
-  )
+  return <AppOriginal />;
 }
 
 export default App;
