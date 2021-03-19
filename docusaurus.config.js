1b8f4c53a5098d47168eb19970ad289d74feb7cc
jen-castiron <jen@castironcoding.com>
2021-03-19 15:27:35 -0700

    [F] Style light dark toggle

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,14 @@
 const path = require("path");
 
+const DARK_ICON = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 14 14'%3E%3Cpath d='M9.8.7c2 1.1 3.4 3.3 3.4 5.8 0 3.7-3 6.7-6.7 6.7-2.5 0-4.7-1.4-5.8-3.4 1 .5 2.1.8 3.3.8 3.7 0 6.7-3 6.7-6.7 0-1.1-.3-2.2-.9-3.2zm-4.2.5c.2-.2.6 0 .5.3l-.4 2.1 1.8 1.1c.3.2.2.6-.1.6l-2.1.2-.5 2c0 .3-.5.4-.6.1l-.8-1.9-2.1.2c-.3 0-.5-.4-.3-.6l1.6-1.4L1.7 2c-.1-.3.2-.6.5-.4l1.8 1 1.6-1.4z' fill-rule='evenodd' clip-rule='evenodd' fill='%237ad3ff'/%3E%3C/svg%3E")`;
+
+const LIGHT_ICON = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath d='M8 4.8C6.2 4.8 4.8 6.2 4.8 8s1.4 3.2 3.2 3.2c1.8 0 3.2-1.4 3.2-3.2S9.8 4.8 8 4.8m0-1.5c.4 0 .7-.3.7-.7V.7C8.7.3 8.4 0 8 0c-.4 0-.7.3-.7.7v1.9c0 .4.3.7.7.7m0 9.4c-.4 0-.7.3-.7.7v1.9c0 .4.3.7.7.7.4 0 .7-.3.7-.7v-1.9c0-.4-.3-.7-.7-.7m-4.3-1.4c.3-.3.7-.3 1 0 .3.3.3.7 0 1l-1.4 1.4c-.1.1-.3.2-.5.2s-.4-.1-.5-.2c-.3-.3-.3-.7 0-1l1.4-1.4zm7.6 0c.3-.3.7-.3 1 0l1.4 1.4c.3.3.3.7 0 1-.1.1-.3.2-.5.2s-.4-.1-.5-.2l-1.4-1.4c-.2-.3-.2-.7 0-1zm4-4c.4 0 .7.3.7.7 0 .4-.3.7-.7.7h-1.9c-.4 0-.7-.3-.7-.7 0-.4.3-.7.7-.7h1.9zm-12.7 0c.4 0 .7.3.7.7 0 .4-.3.7-.7.7H.7C.3 8.7 0 8.4 0 8c0-.4.3-.7.7-.7h1.9zm10.1-5c.3-.3.7-.3 1 0 .3.3.3.7 0 1l-1.4 1.4c-.1.1-.3.2-.5.2s-.4-.1-.5-.2c-.3-.3-.3-.7 0-1l1.4-1.4zm-10.4 0c.3-.3.7-.3 1 0l1.4 1.4c.3.3.3.7 0 1-.2.1-.3.2-.5.2s-.4-.1-.5-.2L2.3 3.3c-.2-.3-.2-.7 0-1z' fill-rule='evenodd' clip-rule='evenodd' fill='%23f5e425'/%3E%3C/svg%3E%0A")`;
+
+const ICON_STYLE = {
+  width: "16px",
+  height: "16px"
+};
+
 module.exports = {
   title: "Manifold Docs",
   tagline: "The tagline of my site",
@@ -146,6 +155,22 @@ module.exports = {
           position: "left"
         }
       ]
+    },
+    colorMode: {
+      switchConfig: {
+        darkIcon: "\u{0020}",
+        darkIconStyle: {
+          ...ICON_STYLE,
+          backgroundImage: DARK_ICON,
+          backgroundRepeat: "no-repeat"
+        },
+        lightIcon: "\u{0020}",
+        lightIconStyle: {
+          ...ICON_STYLE,
+          backgroundImage: LIGHT_ICON,
+          backgroundRepeat: "no-repeat"
+        }
+      }
     }
   },
   presets: [
